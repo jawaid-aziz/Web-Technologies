e725fd66729c7a58378baa6536692bb71443361b
Jawaid Aziz <javaidmemon24@gmail.com>
2025-04-26 19:32:48 +0500

    DOM

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-console.log("hello world");
+const addButton = document.getElementById('add');
 
-console.log("Hello");
+const todoInput = document.getElementById('todo');
+
+const toDoList = document.getElementById('toDoList');
+
+addButton.addEventListener('click', () => {
+    const li = document.createElement('li');
+
+    li.textContent = todoInput.value;
+
+    toDoList.appendChild(li);
+})
